9fe82533749a36a0a75d5ccea2fc403581e7b491
Chandler Goewert <cmgoewert@aol.com>
2017-11-04 15:51:21 -0400

    Added firebase stuffs

--- app/src/main/java/com/example/cmgoe/hackpsu/MainActivity.java
+++ app/src/main/java/com/example/cmgoe/hackpsu/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.cmgoe.hackpsu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
