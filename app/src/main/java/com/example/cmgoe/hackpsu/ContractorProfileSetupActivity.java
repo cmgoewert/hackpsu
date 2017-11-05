cf41d9191b9613cecc7dfd0a666ff2c3b766271a
Luke Pinto <lap5508@gmail.com>
2017-11-05 01:09:54 -0400

    I did a thing!

---
+++ app/src/main/java/com/example/cmgoe/hackpsu/ContractorProfileSetupActivity.java
@@ -0,0 +1,17 @@
+package com.example.cmgoe.hackpsu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lap55 on 11/5/2017.
+ */
+
+public class ContractorProfileSetupActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contractor_profile_skills);
+
+    }
+}
--- app/src/main/java/com/example/cmgoe/hackpsu/ContractorSkillsActivity.java
+++ app/src/main/java/com/example/cmgoe/hackpsu/ContractorSkillsActivity.java
@@ -1,23 +1,12 @@
 package com.example.cmgoe.hackpsu;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.ArrayList;
-
-import Model.Company;
-import Model.FirebaseDB;
 
 /**
  * Created by lap55 on 11/4/2017.
  */
 
 public class ContractorSkillsActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.contractor_profile_skills);
 
-    }
 }
