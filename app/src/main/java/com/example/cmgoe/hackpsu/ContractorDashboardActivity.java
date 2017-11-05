196a58dae28786ed641af398fabf3d41b8b4ff2a
Chandler Goewert <cmgoewert@aol.com>
2017-11-05 11:26:37 -0500

    final commit

--- app/src/main/java/com/example/cmgoe/hackpsu/ContractorDashboardActivity.java
+++ app/src/main/java/com/example/cmgoe/hackpsu/ContractorDashboardActivity.java
@@ -1,8 +1,11 @@
 package com.example.cmgoe.hackpsu;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -10,10 +13,14 @@ import android.widget.TextView;
  */
 
 public class ContractorDashboardActivity extends AppCompatActivity{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contractor_dashboard);
 
     }
+
+
 }
--- app/src/main/java/com/example/cmgoe/hackpsu/ContractorProfileSetupActivity.java
+++ app/src/main/java/com/example/cmgoe/hackpsu/ContractorProfileSetupActivity.java
@@ -3,17 +3,35 @@ package com.example.cmgoe.hackpsu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by lap55 on 11/5/2017.
  */
 
 public class ContractorProfileSetupActivity extends AppCompatActivity{
+    private Button launchDashButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contractor_profile_setup);
+
+        launchDashButton = (Button) findViewById(R.id.portfolio_nav_id);
+
+        launchDashButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                getContractorDash();
+            }
+        });
     }
 
+    public void getContractorDash(){
+        Intent intent = new Intent();
+        intent.setClass(this, ContractorDashboardActivity.class);
+        startActivity(intent);
+    }
 
 }
