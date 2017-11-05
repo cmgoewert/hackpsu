a8a77e6ad57d1c54671740f420fdcee877ec3c28
Luke Pinto <lap5508@gmail.com>
2017-11-05 06:09:39 -0500

    Merge branch 'master' of https://github.com/cmgoewert/hackpsu
    # Conflicts:
    #	app/src/main/java/com/example/cmgoe/hackpsu/ContractorProfileSetupActivity.java
    #	app/src/main/java/com/example/cmgoe/hackpsu/ContractorSkillsActivity.java

---
+++ app/src/main/java/com/example/cmgoe/hackpsu/ContractorDashboardActivity.java
@@ -0,0 +1,19 @@
+package com.example.cmgoe.hackpsu;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by lap55 on 11/5/2017.
+ */
+
+public class ContractorDashboardActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contractor_dashboard);
+
+    }
+}
